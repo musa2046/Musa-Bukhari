32c4480b0c4ed4638633f5d2236128092d018261
musa2046 <musa2046@gmail.com>
2025-08-24 14:20:28 +0500

    correct logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Socials from "./Socials"
 import { NavLink } from "react-router"
+import navLogo from "src/assets/musa_logo_simple.svg"
 
 export default function Navbar(){
     return (
@@ -7,7 +8,7 @@ export default function Navbar(){
             <nav className="navbar d-flex justify-content-between align-items-center p-0" style={{maxHeight:"5vh"}} >
                 <div className="container-fluid">
                     
-                    <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " src="src/assets/musa_logo_simple.svg"></img></a>
+                    <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " src={navLogo}></img></a>
                 
                     <ul className="nav-elements navbar-nav d-flex flex-row justify-content-around gap-5 text-white fw-medium m-0" style={{fontSize :"1.125rem"}}>
                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
@@ -21,4 +22,3 @@ export default function Navbar(){
         </>
     )
 }
-// { ({isActive})=>`nav-link text-white nav-item-underline ${isActive?'active-link':''}`}    
