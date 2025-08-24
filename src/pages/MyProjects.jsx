f35ff47337820b8e5326dc65a0c4e6452a552ef2
musa2046 <musa2046@gmail.com>
2025-08-24 13:58:16 +0500

    getting started with responsiveness

--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import SectionHeading from "../components/SectionHeading";
 import ProjectCard from "../components/ProjectCard";
 import ProgressBar from "../components/ProgressBar";
 import projectsArr from "../projectsArr";
@@ -11,59 +10,59 @@ export default function MyProjects(){
     return(
         <>
             <Navbar />
-            <section className="projects container my-5">
-                <h2 className="fw-bold " >Projects</h2> 
-                <div className=" row row-cols-1 row-cols-md-2 justify-content-center gap-5 py-4" id="moreProjects">
-                
-                    {projectsArr.map(project =>{
-                        return <ProjectCard 
-                            key = {project.key}
-                            heading = {project.heading}
-                            imgURL = {project.imgURL}
-                            // imgURL =  "src\assets\project-pics\Ecommerce Website project.png"
-                            description = {project.description}
-                            previewLink = {project.previewLink}
-                            repoLink = {project.repoLink}
-                        />
-                    })}
-                </div>
-            </section>
-            <section className="container skills" id="moreSkills">
-                <h2 className="fw-bold " >Skills</h2> 
-                <h4 className="my-3 fw-semibold">Web Development</h4>
-                <div className="mx-2">
-                    {skills.map((skill)=>{
-                        return <ProgressBar
-                        name= {skill.name}
-                        level={skill.level}
-                        width={skill.progress} 
-                        key= {skill.key}/>
-                    })}
-                </div>
-            </section>    
-            <section className="container skills"> 
-                <h4 className="my-3 fw-semibold">Programming Languages</h4>
-                <div className="mx-2">
-                    {languages.map((language)=>{
-                        return <ProgressBar
-                        name= {language.name}
-                        level={language.level}
-                        width={language.progress}
-                        key= {language.key}/> 
-                    })}
-                </div>
-
-                <h2 className="fw-bold " >Additional Skills and Technologies</h2> 
-                <div className="container row row-cols-md-3 gx-5 ">
-                    {additionalSkillArr.map((additionalSkill)=>{
-                        return <AdditionalSkill 
-                        key = {additionalSkill.key}
-                        skillName = {additionalSkill.skillName}
-                        />
-                    })}
+                <section className="projects container my-5">
+                    <h2 className="fw-bold " >Projects</h2> 
+                    <div className=" row row-cols-1 row-cols-md-2 justify-content-center gap-5 py-4" id="moreProjects">
                     
-                </div>
-            </section>    
+                        {projectsArr.map(project =>{
+                            return <ProjectCard 
+                                key = {project.key}
+                                heading = {project.heading}
+                                imgURL = {project.imgURL}
+                                // imgURL =  "src\assets\project-pics\Ecommerce Website project.png"
+                                description = {project.description}
+                                previewLink = {project.previewLink}
+                                repoLink = {project.repoLink}
+                            />
+                        })}
+                    </div>
+                </section>
+                <section className="container skills" id="moreSkills">
+                    <h2 className="fw-bold " >Skills</h2> 
+                    <h4 className="my-3 fw-semibold">Web Development</h4>
+                    <div className="mx-2">
+                        {skills.map((skill)=>{
+                            return <ProgressBar
+                            name= {skill.name}
+                            level={skill.level}
+                            width={skill.progress} 
+                            key= {skill.key}/>
+                        })}
+                    </div>
+                </section>    
+                <section className="container skills"> 
+                    <h4 className="my-3 fw-semibold">Programming Languages</h4>
+                    <div className="mx-2">
+                        {languages.map((language)=>{
+                            return <ProgressBar
+                            name= {language.name}
+                            level={language.level}
+                            width={language.progress}
+                            key= {language.key}/> 
+                        })}
+                    </div>
+
+                    <h2 className="fw-bold " >Additional Skills and Technologies</h2> 
+                    <div className="container row row-cols-md-3 gx-5 ">
+                        {additionalSkillArr.map((additionalSkill)=>{
+                            return <AdditionalSkill 
+                            key = {additionalSkill.key}
+                            skillName = {additionalSkill.skillName}
+                            />
+                        })}
+                        
+                    </div>
+                </section>    
             
             <Footer />
         </>
