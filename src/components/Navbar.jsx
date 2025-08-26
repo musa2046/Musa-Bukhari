be4eb91e24a81407aee9a97d4acea2d363f4ff37
musa2046 <musa2046@gmail.com>
2025-08-26 17:06:44 +0500

    navBar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,98 @@
+
 import Socials from "./Socials"
-import { NavLink } from "react-router"
+import { NavLink } from "react-router-dom"
 import navLogo from "../assets/musa_logo_simple.svg"
 
 export default function Navbar(){
     return (
         <>
-            <nav className="navbar d-flex justify-content-between align-items-center p-0" style={{maxHeight:"5vh"}} >
-                <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg  p-0" style={{maxHeight:"5vh"}} >
+                <div className="container-fluid ">
                     
-                    <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " src={navLogo}></img></a>
-                
-                    <ul className="nav-elements navbar-nav d-flex flex-row justify-content-around gap-5 text-white fw-medium m-0" style={{fontSize :"1.125rem"}}>
-                    <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
-                    <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " to="/AboutMe"><span>About Me</span></NavLink></li>
-                    <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/MyProjects"><span>Projects</span></NavLink></li>
-                    </ul>
+                    <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " alt="logo" src={navLogo}></img></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation" data-bs-theme="dark">
+                        <span className="navbar-toggler-icon " ></span>
+                    </button>
+                    
+                    <div className="offcanvas offcanvas-end " tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header" style={{backgroundColor: "#091833"}}>
+                            <h5 className="offcanvas-title text-white" id="offcanvasNavbarLabel">Offcanvas</h5>
+                            <button type="button" className="btn-close me-2" data-bs-theme="dark" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        
 
-                    <Socials />
+                        <div className="offcanvas-body   " style={{backgroundColor: "#091833"}}>
+
+                            <ul className="nav-elements navbar-nav  mx-auto  gap-5 text-white  fw-medium m-0 " style={{fontSize :"1.125rem"}}>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " to="/AboutMe"><span>About Me</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/MyProjects"><span>Projects</span></NavLink></li>
+                            </ul>
+                            
+                            <div className="  d-flex justify-content-center mt-5 mt-lg-0"> <Socials /></div>
+                           
+                        </div>
+                    </div>
                 </div>
             </nav>
         </>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function Navbar(){
+//     return (
+//         <>
+//             <nav className="navbar d-flex justify-content-between align-items-center p-0" style={{maxHeight:"5vh"}} >
+//                 <div className="container-fluid">
+                    
+//                     <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " src={navLogo}></img></a>
+                
+//                     <ul className="nav-elements navbar-nav d-flex flex-row justify-content-around gap-5 text-white fw-medium m-0" style={{fontSize :"1.125rem"}}>
+//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
+//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " to="/AboutMe"><span>About Me</span></NavLink></li>
+//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/MyProjects"><span>Projects</span></NavLink></li>
+//                     </ul>
+
+//                     <Socials />
+//                 </div>
+//             </nav>
+//         </>
+//     )
+// }
