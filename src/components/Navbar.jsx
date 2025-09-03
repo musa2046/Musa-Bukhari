b52e999a3b9b6fed5467c40d1dcc9b4cd6809752
musa2046 <musa2046@gmail.com>
2025-09-03 14:59:55 +0500

    fixed the overflow hidden on phone navigation and added favIcon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import Socials from "./Socials"
 import { NavLink } from "react-router-dom"
-import navLogo from "../assets/musa_logo_simple.svg"
+import navLogo from "../assets/Untitled.svg"
 
 export default function Navbar(){
     return (
@@ -24,10 +24,10 @@ export default function Navbar(){
                         <div className="offcanvas-body   " style={{backgroundColor: "#091833"}}>
 
                             <ul className="nav-elements navbar-nav  mx-auto  gap-5 text-white  fw-medium m-0 " style={{fontSize :"1.125rem"}}>
-                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
-                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " to="/AboutMe"><span>About Me</span></NavLink></li>
-                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/MyProjects"><span>Projects</span></NavLink></li>
-                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/ContactMe"><span>Contact Me</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " data-bs-dismiss="offcanvas"   to="/"><span>Home</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " data-bs-dismiss="offcanvas" to="/AboutMe"><span>About Me</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" data-bs-dismiss="offcanvas" to="/MyProjects"><span>Projects</span></NavLink></li>
+                            <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" data-bs-dismiss="offcanvas" to="/ContactMe"><span>Contact Me</span></NavLink></li>
                             </ul>
                             
                             <div className="  d-flex justify-content-center mt-5 mt-lg-0"> <Socials /></div>
@@ -76,24 +76,3 @@ export default function Navbar(){
 
 
 
-
-// export default function Navbar(){
-//     return (
-//         <>
-//             <nav className="navbar d-flex justify-content-between align-items-center p-0" style={{maxHeight:"5vh"}} >
-//                 <div className="container-fluid">
-                    
-//                     <a href="#" className="navbar-brand d-flex align-items-center p-0 "><img style={{ height: "3vw", maxHeight: "5vw",minHeight:"40px", width: "auto" }} className="img-fluid " src={navLogo}></img></a>
-                
-//                     <ul className="nav-elements navbar-nav d-flex flex-row justify-content-around gap-5 text-white fw-medium m-0" style={{fontSize :"1.125rem"}}>
-//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline "   to="/"><span>Home</span></NavLink></li>
-//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline " to="/AboutMe"><span>About Me</span></NavLink></li>
-//                     <li className="nav-item "> <NavLink className="nav-link text-white nav-item-underline" to="/MyProjects"><span>Projects</span></NavLink></li>
-//                     </ul>
-
-//                     <Socials />
-//                 </div>
-//             </nav>
-//         </>
-//     )
-// }
